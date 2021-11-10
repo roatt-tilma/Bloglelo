19f64de8a5b226ea512de5900d21042f4dc431b7
unknown <umeshsaud2001@gmail.com>
2021-11-11 01:21:54 +0545

    blog database done

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,20 +1,36 @@
 const express = require('express')
+const Blog = require('../models/blog.model')
 const router = express.Router()
 
-router.get('/',(req,res)=>{
-    const blogs = [
-        {
-            title : "Title of Blog",
-            createdAt : new Date(),
-            author : "User currently logged in",
-            description : "Described Content",
-        }
-    ]
+router.get('/',async (req,res)=>{
+    const blogs = await Blog.find().sort({ createdAt: 'desc'})
     res.render('blogs/blogspage',{title : "BLOGS" , blogs : blogs});
 })
 
 router.get('/new',(req,res)=>{
     res.render('blogs/new',{title:"new blog"})
+})
+
+router.get('/:id',async (req,res) => {
+    const blog = await Blog.findById(req.params.id)
+    if(blog==null) res.redirect('/')
+    res.redirect('/blogs')
+})
+
+router.post('/',async (req,res) => {
+    const blog = new Blog({
+        title : req.body.title,
+        description : req.body.description,
+    })
+
+    try{
+        await blog.save()
+        res.redirect(`/blogs/${blog.id}`)
+    }
+    catch(e){
+        res.render('blogs/new');
+    }
+   
 })
 
 module.exports = router
